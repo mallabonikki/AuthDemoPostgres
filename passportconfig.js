990360702bd4e5b2cf37f5efc251bf64abf273a9
C00bra <posta.reduceri@gmail.com>
2016-06-13 15:59:08 +0000

    Add some comments explaining each function in passport.

--- passportconfig.js
+++ passportconfig.js
@@ -2,16 +2,20 @@ var LocalStrategy    = require('passport-local').Strategy,
     Model            = require('./models/model');
 
 module.exports = function(passport) {
+
+    // The result of this done() is stored in the session of HTTP
     passport.serializeUser(function(user, done) {
         done(null, user.id);
     });
 
+    // The result of this done() is stored as req.user
     passport.deserializeUser(function(id, done) {
         Model.grabUserCredentials(id, function(err, user) {
             done(err, user);
         });
     });
 
+    // Tries to log-in
     passport.use(new LocalStrategy(function(username, password, done) {
         Model.logIn(username, password)
             .then(function(user){
