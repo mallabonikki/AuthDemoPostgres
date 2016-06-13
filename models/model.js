830203b98efc519aace1319a6f21a1ddb23cf2c2
C00bra <posta.reduceri@gmail.com>
2016-06-13 15:46:06 +0000

    Switch from bcrypt-nodejs to bcrypt. Clean up a bit.

--- models/model.js
+++ models/model.js
@@ -3,7 +3,7 @@
 var dbBookshelf = require('../dbknex').DB,
     knex = dbBookshelf.knex;
 // Used to encrypt user password before adding it to db.
-var bcrypt = require('bcrypt-nodejs');
+var bcrypt = require('bcrypt');
 
 var User = dbBookshelf.Model.extend({
     tableName: 'users',
@@ -13,15 +13,6 @@ var User = dbBookshelf.Model.extend({
 // ------------------------------
 // createNewUser
 // ------------------------------
-// Makes a new user in the database with
-// automatic incremented ID. Then, returns
-// that user's ID after the user is created.
-// function createNewUser(callback) {
-//     new User().save().then(function(user) {
-//         callback(user.toJSON().id);
-//     });
-// }
-
 function registerNewUser(username, password) {
     return new Promise(function(resolve, reject){
         // Before making the account, try and fetch a username to see if it already exists.
@@ -31,18 +22,40 @@ function registerNewUser(username, password) {
             if (model) {
                 reject("username already exists");
             } else {
-                var hash = bcrypt.hashSync(password);
+                //var salt = bcrypt.genSaltSync(10);
+                var hash = bcrypt.hashSync(password, 10);
 
                 // Make a new postgres db row of the account
                 var signUpUser = new User({ username: username, password: hash });
 
-                signUpUser.save({}, {method: 'insert'}).then(function(model) {
-                    resolve(model);
-                });
+                signUpUser
+                    .save({}, {method: 'insert'})
+                    .then(function(model) {
+                        resolve(model.toJSON());
+                    });
             }
         });
     });
 }
+
+function logIn(username, password) {
+    return new Promise(function(resolve, reject){
+        new User({username: username}).fetch().then(function(data) {
+            var user = data;
+            if (user === null) {
+                reject('Invalid username or password');
+            } else {
+                user = data.toJSON();
+                if (!bcrypt.compareSync(password, user.password)) {
+                    reject('Invalid password');
+                } else {
+                    resolve(user);
+                }
+            }
+        });
+    });
+}
+
 
 // ------------------------------
 // grabUserCredentials
@@ -82,19 +95,19 @@ function grabUserCredentials(userId, callback) {
     });
 }
 
-(function testdb() {
-    knex.select('users.id', 'users.username', 'users.password')
-                .from('users')
-                .then(function(row) {
-        console.log("Knex results: ");
-        console.log(JSON.stringify(row));
-        console.log('--------');
-    });
-})();
+// (function testdb() {
+//     knex.select('users.id', 'users.username', 'users.password')
+//                 .from('users')
+//                 .then(function(row) {
+//         console.log("Knex results: ");
+//         console.log(JSON.stringify(row));
+//         console.log('--------');
+//     });
+// })();
 
 module.exports = {
-    // createNewUser       : createNewUser,
     registerNewUser     : registerNewUser,
     grabUserCredentials : grabUserCredentials,
+    logIn               : logIn,
     User                : User,
 };
--- passportconfig.js
+++ passportconfig.js
@@ -1,6 +1,5 @@
 var LocalStrategy    = require('passport-local').Strategy,
-    Model            = require('./models/model'),
-    bcrypt           = require('bcrypt-nodejs');
+    Model            = require('./models/model');
 
 module.exports = function(passport) {
     passport.serializeUser(function(user, done) {
@@ -14,19 +13,13 @@ module.exports = function(passport) {
     });
 
     passport.use(new LocalStrategy(function(username, password, done) {
-        new Model.User({username: username}).fetch().then(function(data) {
-            var user = data;
-            if (user === null) {
-                return done(null, false, { message: 'Invalid username or password' });
-            } else {
-                user = data.toJSON();
-                if (!bcrypt.compareSync(password, user.password)) {
-                    return done(null, false, { message: 'Invalid password' });
-                } else {
-                    return done(null, user);
-                }
-            }
-        });
+        Model.logIn(username, password)
+            .then(function(user){
+                return done(null, user);
+            })
+            .catch(function(err){
+                return done(null, false, { message: err });
+            });
     }));
 
 };
